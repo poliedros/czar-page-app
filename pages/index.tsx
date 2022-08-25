0cec0bd5191919dfd9064f58405849990cd43cce
Carlos <carloshlzansavio@gmail.com>
2022-08-26 00:05:11 +0100

    install tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Hello World</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 };
