30e579476668c92affa9a86770cefbab16313655
Carlos <carloshlzansavio@gmail.com>
2023-03-12 19:01:44 -0300

    Remove deleted components from index page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,39 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import CzIntro from "./czIntro";
+import Start from "../components/start";
+import Greetings from "../components/greetings";
+import Services from "../components/services";
+import Projects from "../components/projects";
+import Team from "../components/team";
+import Contacts from "../components/contacts";
+import About from "../components/about";
+import Drafts from "../components/drafts";
+import Concepts from "../components/concepts";
+import NavbarMenu from "../components/navbarMenu";
+import NavbarTrans from "../components/navbarTranslations";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import { LanguageProvider } from "../context/languageContext";
 
 const Home: NextPage = () => {
-  //const { user } = useUser({ redirectTo: "/login" });
-
-  //if (!user || user.isLoggedIn == false) {
-  //  return <Spinner />;
-  //}
-
-  return <CzIntro />;
-
-  /* return (
-    <div className="invisibleScroll">
-      <Head>
-        <title>Czar+</title>
-        <meta
-          name="CZAR+ is a software company with a specialized team to develop the right solution for your business."
-          content="Czar+ Page"
-        />
-        <link rel="icon" href="/CZAR+.jpg" />
-      </Head>
-      <div >
-        <CzIntro />
-      </div>
-    </div>
-  ); */
+  return (
+    <>
+      <LanguageProvider>
+        <NavbarMenu />
+        <NavbarTrans />
+        <Start />
+        <Greetings />
+        <Services />
+        <Projects />
+        <Team />
+        <Contacts />
+        <About />
+        <Drafts />
+        <Concepts />
+      </LanguageProvider>
+    </>
+  );
 };
 
 export default Home;
