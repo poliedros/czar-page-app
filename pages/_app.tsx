2e4b10cae51bb5e01b9180a5d282c0490efd5679
Carlos <carloshlzansavio@gmail.com>
2023-04-16 16:36:48 -0300

    Update GA method

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,30 +1,12 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import Script from "next/script";
-import Analytics from "../components/analytics";
-import * as gtag from "./../lib/gtag";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
 
 import Layout from "../components/layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-
-  useEffect(() => {
-    const handleRouteChange = (url: string) => {
-      gtag.pageview(url);
-    };
-    router.events.on("routeChangeComplete", handleRouteChange);
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
-
   return (
     <>
       <Layout>
-        <Analytics />
         <Component {...pageProps} />
       </Layout>
     </>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Script from "next/script";
 import Start from "../components/start";
 import Greetings from "../components/greetings";
 import Services from "../components/services";
@@ -12,6 +13,8 @@ import Concepts from "../components/concepts";
 import NavbarMenu from "../components/navbarMenu";
 import NavbarTrans from "../components/navbarTranslations";
 
+const GA_TRACKING_ID = "G-S1XB734WRP";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import { LanguageProvider } from "../context/languageContext";
@@ -19,6 +22,19 @@ import { LanguageProvider } from "../context/languageContext";
 const Home: NextPage = () => {
   return (
     <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${GA_TRACKING_ID}');
+          `}
+      </Script>
       <LanguageProvider>
         <NavbarMenu />
         <NavbarTrans />
