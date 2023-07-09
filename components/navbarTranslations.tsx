47d92b07cebf6ac8d934f363d6254dde00e98edb
Daniela <daniela.souza.gomes@outlook.com>
2023-07-09 10:25:54 -0300

    Fix navbar style

--- components/navbarTranslations.tsx
+++ components/navbarTranslations.tsx
@@ -49,23 +49,22 @@ export default function NavbarTranslations() {
             <br />
             <Nav.Link
               className="fade-in-rtrans !text-[0.5rem]"
-              style={{ fontSize: ".5rem" }}
+              /* style={{ fontSize: ".5rem" }} */
               onClick={() => toggleLanguage("en")}
             >
               EN
             </Nav.Link>
             <Nav.Link
               className="fade-in-rtrans !text-[0.5rem]"
-              style={{ fontSize: ".5rem" }}
+              /* style={{ fontSize: ".5rem" }} */
               onClick={() => toggleLanguage("es")}
             >
               ES
             </Nav.Link>
             <Nav.Link
               className="fade-in-rtrans !text-[0.5rem]"
-              /* style={{ fontSize: ".5rem" }} */ onClick={() =>
-                toggleLanguage("pt")
-              }
+              /* style={{ fontSize: ".5rem" }} */
+              onClick={() => toggleLanguage("pt")}
             >
               PT
             </Nav.Link>
